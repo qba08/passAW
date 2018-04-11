3d592fa232483b2fd2d9a2832d40dcf101061903
qba08 <jakubh08@gmail.com>
2018-04-11 17:00:06 +0200

    Delete config.js

--- PassWorld/secret/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    dbconnection: 'mongodb://admin:admin@ds050869.mlab.com:50869/onedb',
-    secret: "MyS3cr3tK3Y",
-    keyPath: "./secret/key.pem",
-    certPath: "./secret/cert.pem",
-    smsApiToken:"NVlxp0cBiwhSFM4IRRhVFGlpkIoTslbEFWFkQQRd"
-};
